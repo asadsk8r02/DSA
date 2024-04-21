5887a0efba7c3caec27eccc6ffe16b45ce3d59d2
asadsk8r02 <asadkhanworkmail@gmail.com>
2024-04-20 20:02:15 -0500

    Add1

--- gfg_dump/maths.py
+++ gfg_dump/maths.py
@@ -1,14 +1,26 @@
-def is_palin(n):
-    rev = 0
-    temp = n
+# def is_palin(n):
+#     rev = 0
+#     temp = n
 
-    while temp != 0:
-        ld = temp % 10
-        rev = rev*10+ld
-        temp = temp//10
+#     while temp != 0:
+#         ld = temp % 10
+#         rev = rev*10+ld
+#         temp = temp//10
 
-    return rev == n
+#     return rev == n
 
-if __name__ == '__main__':
-    num = int(input("Enter the number:"))
-    print(is_palin(num))
+# if __name__ == '__main__':
+#     num = int(input("Enter the number:"))
+#     print(is_palin(num))
+
+# def fact(n):
+
+#     if n == 0:
+#         return 1
+    
+#     return n*fact(n-1)
+
+# if __name__ == '__main__':
+#     print (fact(6))
+
+##asad
