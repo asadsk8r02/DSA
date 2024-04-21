2f4b513eb44f310c2cd1bb98fc70fdae8d361d91
asadsk8r02 <asadkhanworkmail@gmail.com>
2024-04-20 19:46:24 -0500

    add

--- gfg_dump/maths.py
+++ gfg_dump/maths.py
@@ -2,4 +2,13 @@ def is_palin(n):
     rev = 0
     temp = n
 
-    while
+    while temp != 0:
+        ld = temp % 10
+        rev = rev*10+ld
+        temp = temp//10
+
+    return rev == n
+
+if __name__ == '__main__':
+    num = int(input("Enter the number:"))
+    print(is_palin(num))
