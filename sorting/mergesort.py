6499aaad2e803c477e158c81da5f2f23ce15ade3
asadsk8r02 <asadkhanworkmail@gmail.com>
2024-06-13 04:08:59 -0500

    Merge sort

--- sorting/mergesort.py
+++ sorting/mergesort.py
@@ -6,5 +6,53 @@
 # In general for arrays, Quicksort outperforms
 #  Not an inplace algo but there are variants of merge sort like BLOCKMERGE SORT wichi are inplace.
 
+# left <= mid < right
 
+# Pseudo code according to GFG part 1 and part2
+# def mergesort(arr,left,right):
+#     if right > left:
+#         mid = (left+right)//2
+#         mergesort(arr,left, mid)
+#         mergesort(arr,mid+1,right)
+#         merge(arr,left, mid, right)
 
+def merge(left, right):
+    merged = []
+    m = len(left)
+    n = len(right)
+    i = 0
+    j = 0
+    while i<m and j<n:
+        if left[i]<=right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+
+    while i<m:
+        merged.append(left[i])
+        i += 1
+    while j<n:
+        merged.append(right[j])
+        j += 1
+
+    return merged
+
+def mergesort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr)//2
+    left = mergesort(arr[:mid])
+    right = mergesort(arr[mid:])
+
+    return merge(left, right)
+
+    
+# Driver code 
+if __name__ == "__main__":
+    arr = [10,30,5,15,7]
+    result = mergesort(arr)
+    print(f"Sorted list is {result}")
+    
