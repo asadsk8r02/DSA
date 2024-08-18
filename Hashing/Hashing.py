71a1b8df211cb190e095d714409eae186fcece07
asadsk8r02 <asadkhanworkmail@gmail.com>
2024-08-18 14:29:30 -0500

    Hashing update

--- Hashing/Hashing.py
+++ Hashing/Hashing.py
@@ -1,1 +1,8 @@
 # Search, Insert, Delete operation ---> O(1) time.
+
+# Not apt for hashing:
+# Finding closest value - AVL tree or Red Black tree
+# Sorted data - AVL tree or Red Black tree
+# Prefix searching - Trie
+
+
